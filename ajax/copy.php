9f62800eb007b1905d629bd626e064bc5dbe3973
claireellul <c.ellul@ucl.ac.uk>
2015-01-02 11:10:37 +0000

    Test differencing
    test differencing

--- ajax/copy.php
+++ ajax/copy.php
@@ -1,17 +1,17 @@
 <?php
-	
+
 	require('dbconnect.php');
 	ini_set("memory_limit","40M");
 	set_time_limit(0);
-	
+echo("test");
 	$phpLayer = '"' . $_POST["layer"] . '"';
 	$phpGeom = "'" . $_POST["geom"] . "'";
-	
-	$addGeomSQL = "INSERT INTO {$phpLayer} (geom) VALUES 
+
+	$addGeomSQL = "INSERT INTO {$phpLayer} (geom) VALUES
 				  (ST_GeomFromText({$phpGeom})) RETURNING \"ID\" ; " ;
-				  
+
 	$copyQuery = pg_query($db, $addGeomSQL);
-	
+
 	while ($copy = pg_fetch_row($copyQuery)) {
 		  echo $copy[0];
 	}
